861772d54125459c85fd60120e2dc9fe9a54f35a
Simon311 <mkontent@gmail.com>
2013-11-08 22:36:31 +0400

    1.2.2.5

--- ChangeName/ChangeName.cs
+++ ChangeName/ChangeName.cs
@@ -4,6 +4,7 @@ using Terraria;
 using TerrariaApi.Server;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 
 namespace ChangeName
 {
@@ -13,7 +14,7 @@ namespace ChangeName
     {
         public override Version Version
         {
-            get { return new Version("1.2.2"); }
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
         }
         public override string Name
         {
@@ -27,12 +28,15 @@ namespace ChangeName
         {
             get { return "Changing names"; }
         }
+
         Dictionary<string, string> oldNames = new Dictionary<string, string>();
+
         public ChangeName(Main game)
             : base(game)
         {
             Order = -1;
         }
+
         public override void Initialize()
         {
             Commands.ChatCommands.Add(new Command("changenames", ChanName, "chname"));
@@ -40,15 +44,17 @@ namespace ChangeName
             Commands.ChatCommands.Add(new Command("selfname", SelfName, "selfname"));
             Commands.ChatCommands.Add(new Command("tshock.canchat", Chat, "chat"));
         }
+
         private void ChanName(CommandArgs args)
         {
-            if( args.Player != null )
+            if (args.Player != null)
             {
                 if (args.Parameters.Count < 2)
                 {
                     args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /chname [player] [newname]");
                     return;
                 }
+
                 var foundplr = TShock.Utils.FindPlayer(args.Parameters[0]);
                 if (foundplr.Count == 0)
                 {
@@ -60,25 +66,20 @@ namespace ChangeName
                     args.Player.SendErrorMessage(string.Format("More than one ({0}) player matched!", args.Parameters.Count));
                     return;
                 }
+
                 string newName = args.Parameters[1];
-                bool hidden;
-                if (args.Parameters.Count<3) { hidden = false; } else { hidden = true; }
+				bool hidden = args.Parameters.Count < 3;
+
                 var plr = foundplr[0];
                 string oldName = plr.TPlayer.name;
-                if (!hidden)
-                {
-                    TShock.Utils.Broadcast(string.Format("{0} has changed {1}'s name to {2}.", args.Player.Name, oldName, newName), Color.DeepPink);
-                    if (TShock.Config.EnableChatAboveHeads) { TShock.Utils.Broadcast("It will take a while before new name appears", Color.DeepPink); }
-                }
-                else
-                {
-                    args.Player.SendMessage(string.Format("You have secretly changed {0}'s name to {1}.", oldName, newName), Color.DeepPink);
-                    if (TShock.Config.EnableChatAboveHeads) { args.Player.SendMessage("It will take a while before new name appears", Color.DeepPink); }
-                }
+                if (!hidden) TShock.Utils.Broadcast(string.Format("{0} has changed {1}'s name to {2}.", args.Player.Name, oldName, newName), Color.DeepPink);
+                else args.Player.SendMessage(string.Format("You have secretly changed {0}'s name to {1}.", oldName, newName), Color.DeepPink);
                 plr.TPlayer.name = newName;
+				NetMessage.SendData((int)PacketTypes.PlayerInfo, -1, -1, plr.TPlayer.name, args.Player.Index, 0, 0, 0, 0);
                 oldNames[newName] = oldName;
             }
         }
+
         private void SelfName(CommandArgs args)
         {
             if (args.Player != null)
@@ -92,11 +93,12 @@ namespace ChangeName
                 string newName = String.Join(" ", args.Parameters);
                 string oldName = plr.TPlayer.name;
                 plr.TPlayer.name = newName;
+				NetMessage.SendData((int)PacketTypes.PlayerInfo, -1, -1, plr.TPlayer.name, args.Player.Index, 0, 0, 0, 0);
                 oldNames[newName] = oldName;
                 TShock.Utils.Broadcast(string.Format("{0} has changed his name to {1}.", oldName, newName), Color.DeepPink);
-                if (TShock.Config.EnableChatAboveHeads) { args.Player.SendMessage("It will take a while before changes apply", Color.DeepPink); }
             }
         }
+
         private void OldName(CommandArgs args)
         {
             if (args.Player != null)
@@ -106,13 +108,12 @@ namespace ChangeName
                     args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /oldname [player]");
                     return;
                 }
-                if(oldNames.ContainsKey(args.Parameters[0])){
-                    args.Player.SendMessage(string.Format("{0}'s old name is {1}.", args.Parameters[0], oldNames[args.Parameters[0]]), Color.DeepPink);
-                } else {
-                    args.Player.SendMessage(string.Format("{0}'s name has not been changed.", args.Parameters[0]), Color.DeepPink);
-                }
+				var name = String.Join(" ", args.Parameters);
+                if (oldNames.ContainsKey(name)) args.Player.SendMessage(string.Format("{0}'s old name is {1}.", name, oldNames[name]), Color.DeepPink);
+				else args.Player.SendMessage(string.Format("{0}'s name has not been changed.", name), Color.DeepPink);
             }
         }
+
         private void Chat(CommandArgs args)
         {
             if (args.Player != null)
@@ -136,6 +137,5 @@ namespace ChangeName
                 }
             }
         }
-
     }
 }
--- ChangeName/Properties/AssemblyInfo.cs
+++ ChangeName/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("92314305-3f43-41fe-9250-fb5aaa42755e")]
-[assembly: AssemblyVersion("1.2.2")]
-[assembly: AssemblyFileVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.2.5")]
+[assembly: AssemblyFileVersion("1.2.2.5")]
